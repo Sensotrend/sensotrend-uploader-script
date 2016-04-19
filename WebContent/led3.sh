54704d2baf6f721b62bc37b040cfbe09055e533e
Mikael Rinnetmäki <mikael.rinnetmaki@gmail.com>
2016-04-19 22:27:55 +0300

    Led scripts from Demola team

---
+++ WebContent/led3.sh
@@ -0,0 +1,18 @@
+#led light
+
+cs /sys/class/gpio
+
+echo 21 > export
+echo "New ledout created"
+cd gpio21
+set out > direction
+echo "GPIO3 set as output"
+for i in 'seq 1 10';
+do
+ echo $i
+ echo 1 > value
+ sleep 1
+ echo 0 > value
+ sleep 1
+done
+echo 1 > value
